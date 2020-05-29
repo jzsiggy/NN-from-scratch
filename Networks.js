78e012c2d0ff60089ba814b0137a1eda568c252d
Joao Zsigmond <jzsiggy@gmail.com>
2020-05-28 21:22:53 -0300

    Feedforward

--- Networks.js
+++ Networks.js
@@ -1,14 +1,34 @@
+const math = require('mathjs');
+
+
 class Network {
   constructor(layers) {
-    this.weights = []
-    this.biases = []
+    this.size = layers.length;
+    this.weights = layers.slice(0, -1).map((layerSize, index) => {
+      const nextLayerSize = layers[index+1]
+      return (
+        [...Array(nextLayerSize)].map(() => {
+          return (
+            math.matrix( [...Array(layerSize)].map(w => Math.random()) )
+          )
+        })
+      );
+    })
+  }
+
+  feedForward (input) {
+    let layer1 = this.weights[0]
+    let layer2Input = layer1.map((weights) => {
+      return math.dot(input, weights) 
+    })
+
+    let layer2 = this.weights[1]
+    let final = layer2.map((weights) => {
+      return math.dot(layer2Input, weights) 
+    })
+    console.log(final)
   }
 }
 
-const sigmoid = (x) => {
-  return Math.exp(x) / (Math.exp(x) + 1)
-};
-
-const dSigmoid = (x) => {
-  return sigmoid(x) * (1 - sigmoid(x))
-}
+const network = new Network([4, 5, 3])
+network.feedForward([1, 2, 3, 4])
