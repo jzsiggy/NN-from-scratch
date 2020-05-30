fc8ef82ba7b917807af9586bc49744eb41055d6f
Joao Zsigmond <jzsiggy@gmail.com>
2020-05-30 00:35:46 -0300

    Adding manual backpropagation

--- Network.js
+++ Network.js
@@ -24,18 +24,65 @@ class Network {
     return activations
   }
 
-  backProp () {
-    
+  backProp (x, y) {
+    const originalCost = this.cost(x, y)
+    let gradientVector = []
+
+    this.weights.forEach((layer, lIndex) => {
+      const dLayer = [];
+      layer.forEach((neuron, nIndex) => {
+        const dNeuron = []
+        neuron.forEach((weight, wIndex) => {
+          let original = weight;
+          this.weights[lIndex][nIndex][wIndex] += 0.01;
+          let newCost = this.cost(x, y)
+          let delWeight = (newCost - originalCost) / 0.01
+          dNeuron.push(delWeight)
+          this.weights[lIndex][nIndex][wIndex] = original
+        })
+        dLayer.push(dNeuron)
+      })
+      gradientVector.push(dLayer);
+    })
+    gradientVector.forEach((layer, lIndex) => {
+      layer.forEach((neuron, nIndex) => {
+        neuron.forEach((del, wIndex) => {
+          this.weights[lIndex][nIndex][wIndex] -= del;
+        })
+      })
+    })
+  };
+
+  cost (x, y) {
+    const activations = this.feedForward(x);
+    const costVector = activations.map((yhat, index) => {
+      return ( yhat - y[index] )**2
+    })
+    return math.sum(costVector);
   }
 
-  cost (x, yhat) {
-    const activations = this.feedForward(x);
-    const costVector = activations.map((val, index) => {
-      return ( val - yhat[index] )**2
-    })
-    return( math.sum(costVector) )
-  }
+  
 }
 
-const network = new Network([1, 1, 1, 1])
-console.log( network.cost([1], [0]) )
+const network = new Network([1, 5, 1])
+console.log(network.weights)
+
+network.train([
+  [1],
+  [1],
+  [1],
+  [1],
+  [0],
+  [0],
+  [0],
+], [
+  [0],
+  [0],
+  [0],
+  [0],
+  [1],
+  [1],
+  [1]
+])
+
+console.log(network.weights)
